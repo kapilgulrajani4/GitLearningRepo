bbfac7817a901ea4a871afe11919054d140461fc
Abhishek sikarwar <46720878+abhi-0297@users.noreply.github.com>
2020-10-01 10:27:55 +0530

    Update for_loops.java

--- for_loops.java
+++ for_loops.java
@@ -52,3 +52,20 @@ public class Example3
           
     } 
 } 
+
+
+public class Example3  
+{ 
+    public static void main(String[] args)  
+    {     
+        // x is integer 
+        int x = 0; 
+        long y = 10; 
+          
+        for (y = 0, x = 1; x < 5; x++)  
+        {  
+            System.out.print(x + " "); 
+        } 
+          
+    } 
+}
