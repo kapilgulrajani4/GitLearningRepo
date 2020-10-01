862070185191a1bc45cc7658c04dc471bb9b542c
Abhishek sikarwar <46720878+abhi-0297@users.noreply.github.com>
2020-10-01 10:21:20 +0530

    Update for_loops.java

---
+++ for_loops.java
@@ -0,0 +1,54 @@
+// Java program to illustrate 
+// infinite loop 
+public class Example1  
+{ 
+    public static void main(String[] args)  
+    { 
+        for( ; ; ) 
+        { 
+            System.out.println("This is an infinite loop"); 
+        } 
+    } 
+  
+}
+
+
+// Java program to illustrate 
+// Initializing multiple variables  
+// in initialization block 
+public class Example2  
+{ 
+  
+    public static void main(String[] args)  
+    { 
+        int x = 2; 
+        for(long y = 0, z = 4; x < 10 && y < 10; x++, y++)  
+        { 
+            System.out.println(y + " "); 
+        } 
+      
+        System.out.println(x); 
+    } 
+}
+
+
+
+
+// Java program to illustrate 
+// redeclaring a variable 
+// in initialization block 
+public class Example3  
+{ 
+    public static void main(String[] args)  
+    {     
+        // x is integer 
+        int x = 0; 
+          
+        // redeclaring x as long will not work 
+        for(long y = 0, x = 1; x < 5; x++)  
+        {  
+            System.out.print(x + " "); 
+        } 
+          
+    } 
+} 
