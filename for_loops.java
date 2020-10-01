702d9923af4c34a9661998ccfb5700d30f8e814d
Abhishek sikarwar <46720878+abhi-0297@users.noreply.github.com>
2020-10-01 10:29:50 +0530

    Update for_loops.java

--- for_loops.java
+++ for_loops.java
@@ -52,3 +52,40 @@ public class Example3
           
     } 
 } 
+
+
+public class Example3  
+{ 
+    public static void main(String[] args)  
+    {     
+        // x is integer 
+        int x = 0; 
+        long y = 10; 
+          
+        for (y = 0, x = 1; x < 5; x++)  
+        {  
+            System.out.print(x + " "); 
+        } 
+          
+    } 
+}
+
+
+// Java program to illustrate 
+// declaring a variable 
+// in initialization block 
+public class Example4  
+{ 
+    public static void main(String[] args)  
+    {     
+        // This will cause error;  
+        // int x; 
+          
+        // redeclaring x as long will not work 
+        for (long y = 0, x = 1; x < 5; x++)  
+        {  
+            System.out.print(x + " "); 
+        } 
+          
+    } 
+} 
